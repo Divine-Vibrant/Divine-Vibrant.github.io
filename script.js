ed56ee15eef4ffd482c6d04c6341fa1b34160912
Divine-Vibrant <devin.silkeutsabay@proton.me>
2024-11-27 14:22:24 -0800

    Initial Commit
    Yay... Hope I can make a free website for me.

---
+++ script.js
@@ -0,0 +1,9 @@
+// My first time doing javascript T_T
+
+function changeImage() {
+    var image = document.getElementById('met');
+    var audio = document.getElementById('a.mp3');
+
+    image.classList.add('hidden');
+    a.play();
+}
