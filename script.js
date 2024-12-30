c3dcbf7df79ba016c44779f5f21acf90bd79a162
Divine-Vibrant <devin.silkeutsabay@proton.me>
2024-12-30 08:05:40 -0800

    Update Default Page

--- script.js
+++
@@ -1,9 +0,0 @@
-// My first time doing javascript T_T
-
-function changeImage() {
-    var image = document.getElementById('met');
-    var audio = document.getElementById('a.mp3');
-
-    image.classList.add('hidden');
-    a.play();
-}
